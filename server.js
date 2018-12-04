3f8e25efc1abf08312b2ed4b3c9285a36afaddd1
Joshua Wiggins <joshua_kar@hotmail.com>
2018-12-04 18:12:58 -0400

    added login,signup and controllers

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
 const path= require('path');
+const bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+const homePageRoute= require('./routes/homepage.js');
+const loginPageRoute = require('./routes/login-page.js');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const homepageRoute= require('./routes/homepage.js');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(homepageRoute);
+app.use(homePageRoute);
+app.use(loginPageRoute);
 app.use('/', (req, res, next)=>{
-	res.render('404', {path: ''});
+	res.render('404',{ 
+		  path: '',
+		  title: '404'
+	    });
 });
 
 app.listen(3000);
