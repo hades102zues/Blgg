bc5413151c0d7502bef48c586241ded61adca7e6
Joshua Wiggins <joshua_kar@hotmail.com>
2018-12-03 21:36:23 -0400

    started using ejs

--- routes/homepage.js
+++ routes/homepage.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 router.get('/',(req, res)=>{
-	res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+	
+	res.render('index', {path:'/'});
 });
 
 
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path= require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const homepageRoute= require('./routes/homepage.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(homepageRoute);
 app.use('/', (req, res, next)=>{
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	res.render('404', {path: ''});
 });
 
 app.listen(3000);
